ecce1abb76bf8d0c07af17c589007d4875e7a85b
christopher <vice7440@gmail.com>
2025-06-14 16:16:26 +0300

    adding migrations

---
+++ server/models/restaurant.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant', lazy=True, cascade="all, delete-orphan")
